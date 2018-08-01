c4e89c7233177a7601749d8ac45ad4b1e5446755
pmanlukas <lukas.pollmann@tum.de>
2018-08-01 14:22:13 +0200

    add new block method

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,38 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-    
-    def new_block(self):
-        #will add a new block to the blockchain
-        pass
+
+        #Create a genisis block
+        self.new_block(#ADD PARAMS)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (optional) <str> Hash of previous block
+        :return: <dict> new Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        #reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender, recipient, amount):
-        #adds new transaction to transactions list
         """
         Creates a new transaction to go into the next mined block
         :param sender: <str> Address of the sender
